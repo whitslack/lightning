13d784d2131569b289155da752110ec3dfd15887
niftynei <niftynei@gmail.com>
2022-07-19 17:04:38 +0930

    bkpr: pass the node_id over for channel_opens, add to account
    it's nice to know what node your channel was opened with. in theory we
    could use listpeers to merge the data after the fact, except that
    channels disappear after they've been closed for a bit. it's better to
    just save the info.
    
    we print it out in `listbalances`, as that's a great place account level
    information

--- lightningd/channel_control.c
+++ lightningd/channel_control.c
@@ -159,6 +159,7 @@ void channel_record_open(struct channel *channel)
 	mvt = new_coin_channel_open(tmpctx,
 				    &channel->cid,
 				    &channel->funding,
+				    &channel->peer->id,
 				    blockheight,
 				    start_balance,
 				    channel->funding_sats,
