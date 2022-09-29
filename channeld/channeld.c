70c12e7429ab047065053e01996fd89bae4960c6
Rusty Russell <rusty@rustcorp.com.au>
2022-09-29 13:19:03 +0930

    onionmessages: remove obsolete onion message parsing.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- channeld/channeld.c
+++ channeld/channeld.c
@@ -2285,7 +2285,6 @@ static void peer_in(struct peer *peer, const u8 *msg)
 	case WIRE_PONG:
 	case WIRE_WARNING:
 	case WIRE_ERROR:
-	case WIRE_OBS2_ONION_MESSAGE:
 	case WIRE_ONION_MESSAGE:
 		abort();
 	}
