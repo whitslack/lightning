ed867221d74896188e61eb03b30ee68e48f10c33
Christian Decker <decker.christian@gmail.com>
2022-10-25 14:51:31 +0200

    msggen: Map the `extratlvs` field of `keysend`
    Changelog-Added: cln-rpc: `keysend` now exposes the `extratlvs` field

--- cln-rpc/src/model.rs
+++ cln-rpc/src/model.rs
@@ -863,10 +863,6 @@ pub mod requests {
 	}
 
 	#[derive(Clone, Debug, Deserialize, Serialize)]
-	pub struct KeysendExtratlvs {
-	}
-
-	#[derive(Clone, Debug, Deserialize, Serialize)]
 	pub struct KeysendRequest {
 	    #[serde(alias = "destination")]
 	    pub destination: PublicKey,
@@ -884,6 +880,8 @@ pub mod requests {
 	    pub exemptfee: Option<Amount>,
 	    #[serde(alias = "routehints", skip_serializing_if = "Option::is_none")]
 	    pub routehints: Option<RoutehintList>,
+	    #[serde(alias = "extratlvs", skip_serializing_if = "Option::is_none")]
+	    pub extratlvs: Option<TlvStream>,
 	}
 
 	impl From<KeysendRequest> for Request {
