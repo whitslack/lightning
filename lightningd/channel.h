71da7c7fb45ee07ff15adde07aa5d038b700c7ac
niftynei <niftynei@gmail.com>
2023-01-09 15:44:26 -0600

    v2 open: if flagged, check that all our inputs are confirmed
    not amazing, since we'll probably call openchannel_update multiple
    times per open, but this is the simplest way to confirm that we're
    not sending unconfirmed outputs to peer.

--- lightningd/channel.h
+++ lightningd/channel.h
@@ -67,6 +67,7 @@ struct open_attempt {
 	struct command *cmd;
 	struct amount_sat funding;
 	const u8 *our_upfront_shutdown_script;
+	bool req_confirmed_ins;
 
 	/* First msg to send to dualopend (to make it create channel) */
 	const u8 *open_msg;
