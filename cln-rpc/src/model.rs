862b7f4df95c4e0f717315a4fe6bc088b594f3f8
Rusty Russell <rusty@rustcorp.com.au>
2023-01-30 16:36:03 +1030

    listtransactions: get rid of per-tx type annotations.
    We didn't actually populate them properly, and the real annotations
    are on inputs and outputs.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Changelog-EXPERIMENTAL: JSON-RPC: `listtransactions` `channel` and `type` field removed at top level.

--- cln-rpc/src/model.rs
+++ cln-rpc/src/model.rs
@@ -2630,8 +2630,6 @@ pub mod responses {
 	    pub rawtx: String,
 	    pub blockheight: u32,
 	    pub txindex: u32,
-	    #[serde(skip_serializing_if = "Option::is_none")]
-	    pub channel: Option<ShortChannelId>,
 	    pub locktime: u32,
 	    pub version: u32,
 	    pub inputs: Vec<ListtransactionsTransactionsInputs>,
