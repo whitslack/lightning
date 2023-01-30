0be5f4e4cdd3300386d23e2e2858ae78cbc00d98
Rusty Russell <rusty@rustcorp.com.au>
2023-01-30 16:54:17 +1030

    doc: use specific types in schema rather than "hex".
    We have "secret" and "hash" types which are often more appropriate.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- cln-rpc/src/model.rs
+++ cln-rpc/src/model.rs
@@ -2358,7 +2358,7 @@ pub mod responses {
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub bolt12: Option<String>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
-	    pub local_offer_id: Option<String>,
+	    pub local_offer_id: Option<Sha256>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub invreq_payer_note: Option<String>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
@@ -3422,7 +3422,7 @@ pub mod responses {
 	}
 	#[derive(Clone, Debug, Deserialize, Serialize)]
 	pub struct ListpaysPays {
-	    pub payment_hash: String,
+	    pub payment_hash: Sha256,
 	    // Path `ListPays.pays[].status`
 	    pub status: ListpaysPaysStatus,
 	    #[serde(skip_serializing_if = "Option::is_none")]
@@ -3437,7 +3437,7 @@ pub mod responses {
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub bolt12: Option<String>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
-	    pub preimage: Option<String>,
+	    pub preimage: Option<Secret>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub number_of_parts: Option<u64>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
