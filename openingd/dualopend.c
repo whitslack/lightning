3c6e92e7519a0de9419bdf7af6ec05a722efebfa
adi2011 <aditya.sharma.met19@itbhu.ac.in>
2023-02-02 20:31:22 +1030

    wire: Add patch file for peer storage bkp
    Add msg type peer_storage and your_peer_storage

--- openingd/dualopend.c
+++ openingd/dualopend.c
@@ -1406,6 +1406,8 @@ static u8 *opening_negotiate_msg(const tal_t *ctx, struct state *state)
 		case WIRE_WARNING:
 		case WIRE_PING:
 		case WIRE_PONG:
+		case WIRE_PEER_STORAGE:
+		case WIRE_YOUR_PEER_STORAGE:
 #if EXPERIMENTAL_FEATURES
 		case WIRE_STFU:
 #endif
@@ -1780,6 +1782,8 @@ static bool run_tx_interactive(struct state *state,
 		case WIRE_REPLY_SHORT_CHANNEL_IDS_END:
 		case WIRE_PING:
 		case WIRE_PONG:
+		case WIRE_PEER_STORAGE:
+		case WIRE_YOUR_PEER_STORAGE:
 #if EXPERIMENTAL_FEATURES
 		case WIRE_STFU:
 #endif
@@ -4062,6 +4066,8 @@ static u8 *handle_peer_in(struct state *state)
 	case WIRE_WARNING:
 	case WIRE_PING:
 	case WIRE_PONG:
+	case WIRE_PEER_STORAGE:
+	case WIRE_YOUR_PEER_STORAGE:
 #if EXPERIMENTAL_FEATURES
 	case WIRE_STFU:
 #endif
