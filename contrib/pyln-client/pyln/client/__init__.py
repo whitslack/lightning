698fafcd851b90d2c88b606214cf722e31114b2d
Michael Schmoock <michael@schmoock.net>
2023-02-27 11:29:17 +0100

    pygossmap: adds GossmapHalfchannel to module exports

--- contrib/pyln-client/pyln/client/__init__.py
+++ contrib/pyln-client/pyln/client/__init__.py
@@ -1,6 +1,6 @@
 from .lightning import LightningRpc, RpcError, Millisatoshi
 from .plugin import Plugin, monkey_patch, RpcException
-from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapNodeId
+from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapHalfchannel, GossmapNodeId
 
 __version__ = "0.11.0"
 
@@ -15,5 +15,6 @@ __all__ = [
     "Gossmap",
     "GossmapNode",
     "GossmapChannel",
+    "GossmapHalfchannel",
     "GossmapNodeId",
 ]
