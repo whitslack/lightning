9ffe3373363f82994560ed6aa674cd2d23ea4ab0
Rusty Russell <rusty@rustcorp.com.au>
2023-03-20 10:47:35 +1030

    db_bind_scid: rename to db_bind_short_channel_id
    We used to have a text version, so this was named 'scid'.  Fix it now.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wallet/wallet.c
+++ wallet/wallet.c
@@ -1301,14 +1301,14 @@ static struct channel *wallet_stmt2channel(struct wallet *w, struct db_stmt *stm
 
 	if (!db_col_is_null(stmt, "alias_local")) {
 		alias[LOCAL] = tal(tmpctx, struct short_channel_id);
-		db_col_scid(stmt, "alias_local", alias[LOCAL]);
+		db_col_short_channel_id(stmt, "alias_local", alias[LOCAL]);
 	} else {
 		alias[LOCAL] = NULL;
 	}
 
 	if (!db_col_is_null(stmt, "alias_remote")) {
 		alias[REMOTE] = tal(tmpctx, struct short_channel_id);
-		db_col_scid(stmt, "alias_remote", alias[REMOTE]);
+		db_col_short_channel_id(stmt, "alias_remote", alias[REMOTE]);
 	} else {
 		alias[REMOTE] = NULL;
 	}
@@ -1968,12 +1968,12 @@ void wallet_channel_save(struct wallet *w, struct channel *chan)
 	db_bind_amount_msat(stmt, 43, &chan->htlc_maximum_msat);
 
 	if (chan->alias[LOCAL] != NULL)
-		db_bind_scid(stmt, 44, chan->alias[LOCAL]);
+		db_bind_short_channel_id(stmt, 44, chan->alias[LOCAL]);
 	else
 		db_bind_null(stmt, 44);
 
 	if (chan->alias[REMOTE] != NULL)
-		db_bind_scid(stmt, 45, chan->alias[REMOTE]);
+		db_bind_short_channel_id(stmt, 45, chan->alias[REMOTE]);
 	else
 		db_bind_null(stmt, 45);
 
@@ -4387,10 +4387,10 @@ void wallet_forwarded_payment_add(struct wallet *w, const struct htlc_in *in,
 	 * the sender used to specify the channel, but that's under
 	 * control of the remote end. */
 	assert(in->key.channel->scid != NULL || in->key.channel->alias[LOCAL]);
-	db_bind_scid(stmt, 2, channel_scid_or_local_alias(in->key.channel));
+	db_bind_short_channel_id(stmt, 2, channel_scid_or_local_alias(in->key.channel));
 
 	if (scid_out)
-		db_bind_scid(stmt, 3, scid_out);
+		db_bind_short_channel_id(stmt, 3, scid_out);
 	else
 		db_bind_null(stmt, 3);
 	db_bind_amount_msat(stmt, 4, &in->msat);
@@ -4513,7 +4513,7 @@ const struct forwarding *wallet_forwarded_payments_get(struct wallet *w,
 	if (chan_in) {
 		// specific in_channel
 		db_bind_int(stmt, 2, 0);
-		db_bind_scid(stmt, 3, chan_in);
+		db_bind_short_channel_id(stmt, 3, chan_in);
 	} else {
 		// any in_channel
 		db_bind_int(stmt, 2, 1);
@@ -4523,7 +4523,7 @@ const struct forwarding *wallet_forwarded_payments_get(struct wallet *w,
 	if (chan_out) {
 		// specific out_channel
 		db_bind_int(stmt, 4, 0);
-		db_bind_scid(stmt, 5, chan_out);
+		db_bind_short_channel_id(stmt, 5, chan_out);
 	} else {
 		// any out_channel
 		db_bind_int(stmt, 4, 1);
@@ -4564,11 +4564,11 @@ const struct forwarding *wallet_forwarded_payments_get(struct wallet *w,
 			cur->payment_hash = NULL;
 		}
 
-		db_col_scid(stmt, "in_channel_scid", &cur->channel_in);
+		db_col_short_channel_id(stmt, "in_channel_scid", &cur->channel_in);
 		cur->htlc_id_in = db_col_u64(stmt, "in_htlc_id");
 
 		if (!db_col_is_null(stmt, "out_channel_scid")) {
-			db_col_scid(stmt, "out_channel_scid", &cur->channel_out);
+			db_col_short_channel_id(stmt, "out_channel_scid", &cur->channel_out);
 		} else {
 			assert(cur->status == FORWARD_LOCAL_FAILED);
 			cur->channel_out.u64 = 0;
@@ -5159,9 +5159,9 @@ struct wallet_htlc_iter *wallet_htlcs_next(struct wallet *w,
 		*scid = iter->scid;
 	else {
 		if (db_col_is_null(iter->stmt, "channels.scid"))
-			db_col_scid(iter->stmt, "channels.alias_local", scid);
+			db_col_short_channel_id(iter->stmt, "channels.alias_local", scid);
 		else {
-			db_col_scid(iter->stmt, "channels.scid", scid);
+			db_col_short_channel_id(iter->stmt, "channels.scid", scid);
 			db_col_ignore(iter->stmt, "channels.alias_local");
 		}
 	}
