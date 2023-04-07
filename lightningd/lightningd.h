1a94ca140ff13e531e0117ae08f2f340fd92d99f
Rusty Russell <rusty@rustcorp.com.au>
2023-04-07 14:09:05 +0930

    plugins/bcli: move commit-fee (dev-max-fee-multiplier) and into core.
    Turns out the two bcli replacements I checked (`sauron` and
    `trustedcoin`) don't even implement this, and the multiplier makes
    more sense in lightningd, especially as we move to bcli just providing
    raw feerate estimates.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/lightningd.h
+++ lightningd/lightningd.h
@@ -82,6 +82,13 @@ struct config {
 	 * slight spec incompatibility, but implementations do this
 	 * already. */
 	bool allowdustreserve;
+
+	/* The factor to time the urgent feerate by to get the maximum
+	 * acceptable feerate.  (10, but can be overridden by dev-max-fee-multiplier) */
+	u32 max_fee_multiplier;
+
+	/* Percent of CONSERVATIVE/2 feerate we'll use for commitment txs. */
+	u64 commit_fee_percent;
 };
 
 typedef STRMAP(const char *) alt_subdaemon_map;
