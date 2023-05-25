e5d9afe3429a190bfefad145e6b0cbbfd9d0ab89
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-05-25 19:58:57 -0400

    Revert "lightningd: re-add 'offerout' functionality, as 'invoicerequest'."
    This reverts commit 37bc4603b8c088eecd67863071157e3729d90470.

--- lightningd/offer.c
+++ lightningd/offer.c
@@ -396,7 +396,7 @@ static struct command_result *json_createinvoicerequest(struct command *cmd,
 	struct json_stream *response;
 	u64 *prev_basetime = NULL;
 	struct sha256 merkle;
-	bool *save, *single_use, *exposeid;
+	bool *save, *single_use;
 	enum offer_status status;
 	struct sha256 invreq_id;
 	const char *b12str;
@@ -404,7 +404,6 @@ static struct command_result *json_createinvoicerequest(struct command *cmd,
 	if (!param(cmd, buffer, params,
 		   p_req("bolt12", param_b12_invreq, &invreq),
 		   p_req("savetodb", param_bool, &save),
-		   p_opt_def("exposeid", param_bool, &exposeid, false),
 		   p_opt("recurrence_label", param_label, &label),
 		   p_opt_def("single_use", param_bool, &single_use, true),
 		   NULL))
@@ -450,14 +449,10 @@ static struct command_result *json_createinvoicerequest(struct command *cmd,
 	}
 
 	invreq->invreq_payer_id = tal(invreq, struct pubkey);
-	if (*exposeid) {
-		if (!pubkey_from_node_id(invreq->invreq_payer_id,
-					 &cmd->ld->id))
-			fatal("Our ID is invalid?");
-	} else if (!payer_key(cmd->ld,
-			      invreq->invreq_metadata,
-			      tal_bytelen(invreq->invreq_metadata),
-			      invreq->invreq_payer_id)) {
+	if (!payer_key(cmd->ld,
+		       invreq->invreq_metadata,
+		       tal_bytelen(invreq->invreq_metadata),
+		       invreq->invreq_payer_id)) {
 		return command_fail(cmd, JSONRPC2_INVALID_PARAMS,
 				    "Invalid tweak");
 	}
@@ -471,8 +466,8 @@ static struct command_result *json_createinvoicerequest(struct command *cmd,
 	merkle_tlv(invreq->fields, &merkle);
 	invreq->signature = tal(invreq, struct bip340sig);
 	hsm_sign_b12(cmd->ld, "invoice_request", "signature",
-		     &merkle, *exposeid ? NULL : invreq->invreq_metadata,
-		     invreq->invreq_payer_id, invreq->signature);
+		     &merkle, invreq->invreq_metadata, invreq->invreq_payer_id,
+		     invreq->signature);
 
 	b12str = invrequest_encode(cmd, invreq);
 
