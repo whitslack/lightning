c060c94906ae4580906591001b0484e4eb613b4b
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-05-25 19:59:57 -0400

    Revert "autoclean: fix uncleaned stats when we don't clean due to being too new."
    This reverts commit 24d7aad3d0ae16995056f489fbe675b7f01d75ff.

--- plugins/autoclean.c
+++ plugins/autoclean.c
@@ -223,8 +223,7 @@ static struct command_result *listinvoices_done(struct command *cmd,
 			json_add_tok(req->js, "label", label, buf);
 			json_add_tok(req->js, "status", status, buf);
 			send_outreq(plugin, req);
-		} else
-			cinfo->num_uncleaned++;
+		}
 	}
 
 	if (cinfo->cleanup_reqs_remaining)
