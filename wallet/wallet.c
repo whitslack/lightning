9faf0eb5c8f016597860cec4ee9d47085b23d486
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-05-25 19:59:42 -0400

    Revert "wallet: fix typo in debug message."
    This reverts commit 9e41754e7d23f7e79e58ebdd7170889aaacd902d.

--- wallet/wallet.c
+++ wallet/wallet.c
@@ -2358,7 +2358,7 @@ int wallet_extract_owned_outputs(struct wallet *w, const struct wally_tx *wtx,
 			  type_to_string(tmpctx, struct bitcoin_txid,
 					 &utxo->outpoint.txid),
 			  blockheight ? " CONFIRMED" : "",
-			  is_coinbase ? " COINBASE" : "");
+			  is_coinbase == 0 ? " COINBASE" : "");
 
 		/* We only record final ledger movements */
 		if (blockheight) {
