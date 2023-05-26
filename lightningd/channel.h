131036a75666ad1fb709e0b67646d9c68ff9542c
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-05-25 20:55:13 -0400

    Revert "df: persist channel open preference to database"
    This reverts commit f05d4500982c142a47f8ab2dd4bca257d42862d3.

--- lightningd/channel.h
+++ lightningd/channel.h
@@ -70,6 +70,7 @@ struct open_attempt {
 	struct command *cmd;
 	struct amount_sat funding;
 	const u8 *our_upfront_shutdown_script;
+	bool req_confirmed_ins;
 
 	/* First msg to send to dualopend (to make it create channel) */
 	const u8 *open_msg;
@@ -118,9 +119,6 @@ struct channel {
 
 	/* Our channel config. */
 	struct channel_config our_config;
-
-	/* Require confirmed inputs for interactive tx */
-	bool req_confirmed_ins;
 
 	/* Minimum funding depth (specified by us if they fund). */
 	u32 minimum_depth;
@@ -286,7 +284,6 @@ struct channel *new_channel(struct peer *peer, u64 dbid,
 			    struct log *log STEALS,
 			    const char *transient_billboard TAKES,
 			    u8 channel_flags,
-			    bool req_confirmed_ins_remote,
 			    const struct channel_config *our_config,
 			    u32 minimum_depth,
 			    u64 next_index_local,
