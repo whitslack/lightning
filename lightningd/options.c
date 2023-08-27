58a99bbdc28d863d44d90cd602efbdd5ed7721c1
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-08-26 20:44:46 -0400

    Revert "splicing: Remove dependency on experimental_dual_fund"
    This reverts commit fb179f2d5eb66469beabecfc9a327fbedc968fd9.

--- lightningd/options.c
+++ lightningd/options.c
@@ -1197,6 +1197,8 @@ static char *opt_set_splicing(struct lightningd *ld)
 	feature_set_or(ld->our_features,
 		       take(feature_set_for_feature(NULL,
 						    OPTIONAL_FEATURE(OPT_SPLICE))));
+	/* Splicing requires dual-fund to be enabled */
+	opt_set_dual_fund(ld);
 	return NULL;
 }
 
--- tests/utils.py
+++ tests/utils.py
@@ -43,6 +43,7 @@ def expected_peer_features(wumbo_channels=False, extra=[]):
         # option_dual_fund
         features += [29]
     if EXPERIMENTAL_SPLICING:
+        features += [29]  # option_dual_fund
         features += [35]  # option_quiesce
         features += [63]  # option_splice
     return hex_bits(features + extra)
@@ -59,6 +60,7 @@ def expected_node_features(wumbo_channels=False, extra=[]):
         # option_dual_fund
         features += [29]
     if EXPERIMENTAL_SPLICING:
+        features += [29]  # option_dual_fund
         features += [35]  # option_quiesce
         features += [63]  # option_splice
     return hex_bits(features + extra)
