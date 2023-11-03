2186950c336058faf1238e9d6772d7f670b1f504
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-11-03 17:42:28 -0400

    Revert "wallet: add last_used_nsec in runes table"
    This reverts commit eacf0b502c28662b515eaec2f3e975ac51423f20.

--- wallet/wallet.h
+++ wallet/wallet.h
@@ -1585,19 +1585,17 @@ struct wally_psbt *psbt_using_utxos(const tal_t *ctx,
  * @ctx: tal ctx for return to be tallocated from
  * @wallet: the wallet
  * @unique_id: the id of the rune.
- * @last_used: absolute time rune was last used
  *
  * Returns NULL if it's not found.
  */
-const char *wallet_get_rune(const tal_t *ctx, struct wallet *wallet, u64 unique_id, struct timeabs *last_used);
+const char *wallet_get_rune(const tal_t *ctx, struct wallet *wallet, u64 unique_id);
 
 /**
  * Get every runestring from the db
  * @ctx: tal ctx for return to be tallocated from
  * @wallet: the wallet
- * @last_used: absolute time rune was last used
  */
-const char **wallet_get_runes(const tal_t *ctx, struct wallet *wallet, struct timeabs **last_used);
+const char **wallet_get_runes(const tal_t *ctx, struct wallet *wallet);
 
 /**
  * wallet_rune_insert -- Insert the newly created rune into the database
