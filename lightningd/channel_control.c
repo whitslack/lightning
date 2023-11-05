7cf4d09a0765263fdd6f37f88dc01243a8801055
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-11-05 15:03:04 -0500

    Revert "splice: Fixes from splice-out test"
    This reverts commit 0a5ef7f2e66266006928b6b646acae960f90ab5a.

--- lightningd/channel_control.c
+++ lightningd/channel_control.c
@@ -192,20 +192,24 @@ static void handle_splice_funding_error(struct lightningd *ld,
 
 	cc = splice_command_for_chan(ld, channel);
 	if (cc) {
-		was_pending(command_fail(cc->cmd, SPLICE_FUNDING_LOW,
-					 "%s provided %s but committed to %s.",
-					 opener_error ? "You" : "Peer",
-					 fmt_amount_msat(tmpctx, funding),
-					 fmt_amount_msat(tmpctx, req_funding)));
+		struct json_stream *response = json_stream_success(cc->cmd);
+		json_add_string(response, "message", "Splice funding too low");
+		json_add_string(response, "error",
+				tal_fmt(tmpctx,
+					"%s provided %s but committed to %s.",
+					opener_error ? "You" : "Peer",
+					fmt_amount_msat(tmpctx, funding),
+					fmt_amount_msat(tmpctx, req_funding)));
+
+		was_pending(command_success(cc->cmd, response));
 	}
-	else {
+	else
 		log_peer_unusual(ld->log, &channel->peer->id,
 				 "Splice funding too low. %s provided but %s"
 				 " commited to %s",
 				 opener_error ? "peer" : "you",
 				 fmt_amount_msat(tmpctx, funding),
 				 fmt_amount_msat(tmpctx, req_funding));
-	}
 }
 
 static void handle_splice_state_error(struct lightningd *ld,
@@ -223,9 +227,13 @@ static void handle_splice_state_error(struct lightningd *ld,
 	}
 
 	cc = splice_command_for_chan(ld, channel);
-	if (cc)
-		was_pending(command_fail(cc->cmd, SPLICE_STATE_ERROR,
-					 "%s", error_msg));
+	if (cc) {
+		struct json_stream *response = json_stream_success(cc->cmd);
+		json_add_string(response, "message", "Splice state error");
+		json_add_string(response, "error", error_msg);
+
+		was_pending(command_success(cc->cmd, response));
+	}
 	else
 		log_peer_unusual(ld->log, &channel->peer->id,
 				 "Splice state error: %s", error_msg);
@@ -238,7 +246,6 @@ static void handle_splice_feerate_error(struct lightningd *ld,
 	struct splice_command *cc;
 	struct amount_msat fee;
 	bool too_high;
-	char *error_msg;
 
 	if (!fromwire_channeld_splice_feerate_error(msg, &fee, &too_high)) {
 		channel_internal_error(channel,
@@ -249,25 +256,27 @@ static void handle_splice_feerate_error(struct lightningd *ld,
 
 	cc = splice_command_for_chan(ld, channel);
 	if (cc) {
-		if (too_high)
-			error_msg = tal_fmt(tmpctx, "Feerate too high. Do you "
-				      "really want to spend %s on fees?",
-				      fmt_amount_msat(tmpctx, fee));
-		else
-			error_msg = tal_fmt(tmpctx, "Feerate too low. Your "
-				      "funding only provided %s in fees",
-				      fmt_amount_msat(tmpctx, fee));
+		struct json_stream *response = json_stream_success(cc->cmd);
+		json_add_string(response, "message", "Splice feerate failed");
 
-		was_pending(command_fail(cc->cmd,
-					 too_high ? SPLICE_HIGH_FEE : SPLICE_LOW_FEE,
-					 "%s", error_msg));
+		if (too_high)
+			json_add_string(response, "error",
+					tal_fmt(tmpctx, "Feerate too high. Do you "
+						"really want to spend %s on fees?",
+						fmt_amount_msat(tmpctx, fee)));
+		else
+			json_add_string(response, "error",
+					tal_fmt(tmpctx, "Feerate too low. Your "
+						"funding only provided %s in fees",
+						fmt_amount_msat(tmpctx, fee)));
+
+		was_pending(command_success(cc->cmd, response));
 	}
-	else {
+	else
 		log_peer_unusual(ld->log, &channel->peer->id, "Peer gave us a"
 				 " splice pkg with too low of feerate (fee was"
 				 " %s), we rejected it.",
 				 fmt_amount_msat(tmpctx, fee));
-	}
 }
 
 /* When channeld finishes processing the `splice_init` command, this is called */
@@ -1408,10 +1417,7 @@ bool peer_start_channeld(struct channel *channel,
 		infcopy->outpoint = inflight->funding->outpoint;
 		infcopy->amnt = inflight->funding->total_funds;
 		infcopy->splice_amnt = inflight->funding->splice_amnt;
-		if (inflight->last_tx)
-			infcopy->last_tx = tal_dup(infcopy, struct bitcoin_tx, inflight->last_tx);
-		else
-			infcopy->last_tx = NULL;
+		infcopy->last_tx = tal_dup(infcopy, struct bitcoin_tx, inflight->last_tx);
 		infcopy->last_sig = inflight->last_sig;
 		infcopy->i_am_initiator = inflight->i_am_initiator;
 		tal_wally_start();
