658961278f8d3d1db02b347c796dc45f6b3d6d07
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-11-05 13:51:35 -0500

    Revert "lightningd: add ordering and pagination to listsendpays."
    This reverts commit c70a326f1f789f8ad693e8fec5bb64291660d5d0.

--- contrib/pyln-client/pyln/client/lightning.py
+++ contrib/pyln-client/pyln/client/lightning.py
@@ -1097,15 +1097,12 @@ class LightningRpc(UnixDomainSocketRpc):
         }
         return self.call("listpeerchannels", payload)
 
-    def listsendpays(self, bolt11=None, payment_hash=None, status=None, index=None, start=None, limit=None):
+    def listsendpays(self, bolt11=None, payment_hash=None, status=None):
         """Show all sendpays results, or only for `bolt11` or `payment_hash`."""
         payload = {
             "bolt11": bolt11,
             "payment_hash": payment_hash,
-            "status": status,
-            "index": index,
-            "start": start,
-            "limit": limit,
+            "status": status
         }
         return self.call("listsendpays", payload)
 
--- wallet/wallet.h
+++ wallet/wallet.h
@@ -1024,17 +1024,11 @@ void wallet_payment_set_failinfo(struct wallet *wallet,
 /**
  * payments_first: get first payment, optionally filtering by status
  * @w: the wallet
- * @listindex: what index order to use (if you care)
- * @liststart: first index to return (0 == all).
- * @listlimit: limit on number of entries to return (NULL == no limit).
  *
  * Returns NULL if none, otherwise you must call payments_next() or
  * tal_free(stmt).
  */
-struct db_stmt *payments_first(struct wallet *w,
-			       const enum wait_index *listindex,
-			       u64 liststart,
-			       const u32 *listlimit);
+struct db_stmt *payments_first(struct wallet *w);
 
 /**
  * payments_next: get next payment
@@ -1063,18 +1057,12 @@ struct db_stmt *payments_by_hash(struct wallet *w,
  * payments_by_status: get the payments, if any, by status.
  * @w: the wallet
  * @status: the status.
- * @listindex: what index order to use (if you care)
- * @liststart: first index to return (0 == all).
- * @listlimit: limit on number of entries to return (NULL == no limit).
  *
  * Returns NULL if none, otherwise call payments_get_details(),
  * and then tal_free(stmt).
  */
 struct db_stmt *payments_by_status(struct wallet *w,
-				   enum payment_status status,
-				   const enum wait_index *listindex,
-				   u64 liststart,
-				   const u32 *listlimit);
+				   enum payment_status status);
 
 /**
  * payments_by_label: get the payment, if any, by label.
