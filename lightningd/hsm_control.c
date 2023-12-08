d022a17f00838a4ef7a39d79c78f28f10df1a1f3
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-12-08 05:48:32 -0500

    Revert "splice: fix WIRE_HSMD_SIGN_SPLICE_TX capability check"
    This reverts commit 9e0b244058e699a57b018ce4fae7d6f0c49a3ad3.

--- lightningd/hsm_control.c
+++ lightningd/hsm_control.c
@@ -167,7 +167,7 @@ struct ext_key *hsm_init(struct lightningd *ld)
 	}
 
 	if (feature_offered(ld->our_features->bits[INIT_FEATURE],
-			    OPT_EXPERIMENTAL_SPLICE)
+			    WIRE_HSMD_SIGN_SPLICE_TX)
 	    && !hsm_capable(ld, WIRE_HSMD_SIGN_SPLICE_TX)) {
 		fatal("--experimental-splicing needs HSM capable of signing splices!");
 	}
