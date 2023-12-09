324fa20ad966c1ffb1fa2c20d5940c42e1db0888
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-12-09 01:30:11 -0500

    Revert "track fallbacks for invoices, pt 1"
    This reverts commit cdc0bd47ff9aa15e5550f50b558d7cb91b72b0cd.

--- lightningd/watch.c
+++ lightningd/watch.c
@@ -21,9 +21,6 @@
  *   - HTLC timed out
  *   - HTLC spent
  *
- * - Payments to invoice fallback addresses:
- *   - Reached a given depth.
- *
  * We do this by adding the P2SH address to the wallet, and then querying
  * that using listtransactions.
  *
