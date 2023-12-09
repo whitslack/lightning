1805ee9dea9ec592f2634c53b607abe62e616681
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-12-09 01:37:36 -0500

    Revert "display paid_outpoint on invoices when present"
    This reverts commit 54ef84e8915f2618cc98fe003ef09825606d64e3.

--- wallet/invoices.h
+++ wallet/invoices.h
@@ -204,15 +204,13 @@ struct db_stmt *invoices_next(struct invoices *invoices,
  * @inv_dbid - the invoice to mark as paid.
  * @received - the actual amount received.
  * @label    - the label of the invoice.
- * @outpoint - the outpoint (if onchain).
  *
  * If the invoice is not UNPAID, returns false.
  */
 bool invoices_resolve(struct invoices *invoices,
 		      u64 inv_dbid,
 		      struct amount_msat received,
-		      const struct json_escape *label,
-		      const struct bitcoin_outpoint *outpoint);
+		      const struct json_escape *label);
 
 /**
  * invoices_waitany - Wait for any invoice to be paid.
